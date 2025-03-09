1d000a8fe6bdacaa3c27accf1ae124523d93d0c7
Sudhanshu Pandey <sp6370@nyu.edu>
2025-03-09 13:05:28 -0400

    chore: phase 1 complete

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,14 @@
+use std::panic::resume_unwind;
+
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    let mut result  = 1;
+
+    for i in 1..=n{
+        result =  result * i
+    }
+
+    result
 }
 
 #[cfg(test)]
